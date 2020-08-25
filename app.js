3654a7c05f7f1e32a25c13a69faf716f47ed94af
Benjamin Brier <ben.brier14@gmail.com>
2020-08-25 18:14:47 -0400

    app complete

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ function deleteCheck(e) {
 		const todo = item.parentElement;
 		//Animation
 		todo.classList.add('fall');
+		removeLocalTodos(todo);
 		todo.addEventListener('transitionend', function() {
 			todo.remove();
 		});
@@ -124,4 +125,16 @@ function getTodos() {
 		//Append List
 		todoList.appendChild(todoDiv);
 	});
+}
+
+function removeLocalTodos(todo) {
+	let todos;
+	if (localStorage.getItem('todos') === null) {
+		todos = [];
+	} else {
+		todos = JSON.parse(localStorage.getItem('todos'));
+	}
+	const todoIndex = todo.children[0].innerText;
+	todos.splice(todos.indexOf(todoIndex), 1);
+	localStorage.setItem('todos', JSON.stringify(todos));
 }
